7c640e372f13ddcae4332dd975839c33bd733cc9
Hansel Tritama <hanseltritama@lmhanse28337711.home>
2019-01-04 00:20:41 -0800

    added Geocoder

--- app/src/main/java/com/hanseltritama/maplocation/MapsActivity.java
+++ app/src/main/java/com/hanseltritama/maplocation/MapsActivity.java
@@ -3,6 +3,8 @@ package com.hanseltritama.maplocation;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -11,6 +13,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
@@ -20,6 +23,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -79,7 +86,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
                 mMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location"));
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 5));
+
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+                try {
+
+                    List<Address> addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+                    if(addressList != null && addressList.size() > 0) {
+
+                        Toast.makeText(MapsActivity.this,
+                                addressList.get(0).getCountryName().toString() + ", "
+                                + addressList.get(0).getCountryCode().toString(),
+                                Toast.LENGTH_SHORT).show();
+
+                    }
+
+                } catch (IOException e) {
+
+                    e.printStackTrace();
+
+                }
 
             }
 
